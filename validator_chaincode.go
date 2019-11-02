4baadfe6af57924f6b424f56d715be401c59fac3
Oleg Abdrashitov <olegabu73@gmail.com>
2019-11-02 15:51:33 +0300

    tests consume actual transaction

--- validator_chaincode.go
+++ validator_chaincode.go
@@ -14,21 +14,31 @@ type ValidatorChaincode struct {
 
 // Init is called when the chaincode is instantiated by the blockchain network.
 func (cc *ValidatorChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	_, params := stub.GetFunctionAndParameters()
-	logger.Info(params)
+	function, params := stub.GetFunctionAndParameters()
+	logger.Info(function, params)
 	return shim.Success(nil)
 }
 
 // Invoke is called as a result of an application request to run the chaincode.
 func (cc *ValidatorChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, params := stub.GetFunctionAndParameters()
-	logger.Info(function)
-	logger.Info(params)
+	logger.Info(function, params)
 
-	err := mw.VerifySignature([]byte(params[0]))
-	if err != nil {
-		return shim.Error(err.Error())
+	txBytes := []byte(params[0])
+
+	if function == "validate" {
+		err := mw.ValidateSignature(txBytes)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		//err = mw.ValidateCommitmentsSum(txBytes)
+		//if err != nil {
+		//	return shim.Error(err.Error())
+		//}
+
+		return shim.Success(nil)
 	}
 
-	return shim.Success(nil)
+	return shim.Error("unknown function")
 }
--- validator_chaincode_test.go
+++ validator_chaincode_test.go
@@ -3,13 +3,34 @@ package chaincode
 import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"log"
 	"testing"
 )
+
+func getTxBytes() []byte {
+	bytes, err := ioutil.ReadFile("1g_repost_fix_kernel.json")
+	if err != nil {
+		log.Panic("cannot open json file with test transaction")
+	}
+
+	return bytes
+}
 
 func TestInit(t *testing.T) {
 	cc := new(ValidatorChaincode)
 	stub := shim.NewMockStub("chaincode", cc)
 	res := stub.MockInit("1", [][]byte{[]byte("init")})
+	log.Print(res)
 
-	assert.Equal(t, shim.OK, res.Status)
+	assert.EqualValues(t, shim.OK, res.Status)
+}
+
+func TestValidate(t *testing.T) {
+	cc := new(ValidatorChaincode)
+	stub := shim.NewMockStub("chaincode", cc)
+	res := stub.MockInvoke("1", [][]byte{[]byte("validate"), getTxBytes()})
+	log.Print(res)
+
+	assert.EqualValues(t, shim.OK, res.Status)
 }
